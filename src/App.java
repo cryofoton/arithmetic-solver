c1c77cf9c58eaf10ee10bd39bcd6465bafcb1ea8
Osman Sulaiman <sb.osman@outlook.my>
2022-09-10 21:55:17 +0800

    Add code  to process expression

--- src/App.java
+++ src/App.java
@@ -1,9 +1,84 @@
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Stack;
+
 public class App {
 
     public static double calculate(String sum) {
-        // Your code starts here
 
-        return 0;
+        // trim space and bracket at prefix and suffix
+        String expr = sum.trim();
+        if (expr.startsWith("(") && expr.endsWith(")")) {
+            expr = expr.substring(1, expr.length() - 1).trim();
+        }
+
+        // convert the expression to be solved into array
+        String[] exprArr = expr.split(" ");
+        List<String> toSolve = new ArrayList<>();
+
+        // process the bracket
+        Stack<Integer> bracketIdx = new Stack<>();
+        for (int i = 0; i < exprArr.length; i++) {
+            String item = exprArr[i];
+            toSolve.add(item);
+            if ("(".equals(item)) {
+                bracketIdx.push(i);
+            } else if (")".equals(item)) {
+                int openIdx = bracketIdx.pop();
+                List<String> toEval = toSolve.subList(openIdx, toSolve.size());
+                double solved = calculate(String.join(" ", toEval));
+                toEval.clear();
+                toSolve.add(String.valueOf(solved));
+            }
+        }
+
+        // handle multiplication and division
+        Queue<String> inProcess = new LinkedList<>();
+        String operator = null;
+        BigDecimal left = null;
+        for (String num: toSolve) {
+            try {
+                BigDecimal operand = new BigDecimal(num);
+                if (left == null) {
+                    left = operand;
+                } else {
+                    if ("*".equals(operator)) {
+                        left = left.multiply(operand);
+                    } else if ("/".equals(operator)) {
+                        left = left.divide(operand);
+                    } else {
+                        inProcess.add(left.toPlainString());
+                        inProcess.add(operator);
+                        left = operand;
+                    }
+                }
+            } catch (NumberFormatException e) {
+                operator = num;
+            }
+        }
+        inProcess.add(left.toPlainString());
+
+        // handle addition and substraction
+        BigDecimal result = BigDecimal.ZERO;
+        operator = "+";
+        while (!inProcess.isEmpty()) {
+            String elem = inProcess.remove();
+            try {
+                BigDecimal value = new BigDecimal(elem);
+                if ("+".equals(operator)) {
+                    result = result.add(value);
+                } else if ("-".equals(operator)) {
+                    result = result.subtract(value);
+                }
+            } catch (NumberFormatException e) {
+                operator = elem;
+            }
+        }
+
+        return result.doubleValue();
     }
 
     public static void main(String[] args) throws Exception {
