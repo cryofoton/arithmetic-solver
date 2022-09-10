3237e2cf17177f6ccfb9a08cc61dc0308fe52d46
Osman Sulaiman <sb.osman@outlook.my>
2022-09-10 22:44:24 +0800

    improve result print output

--- src/App.java
+++ src/App.java
@@ -82,43 +82,53 @@ public class App {
     }
 
     public static void main(String[] args) throws Exception {
-        System.out.println("Solve below arithmetic: \n");
+        System.out.println("Solve below arithmetic: ");
+        System.out.println();
 
         String q1 = "1 + 1";
         System.out.println(q1);
-        System.out.println("Expected: 2.0 -> result: " + calculate(q1) + "\n");
+        System.out.println("Expected: 2.0 -> result: " + calculate(q1));
+        System.out.println();
 
         String q2 = "2 * 2";
         System.out.println(q2);
-        System.out.println("Expected: 4.0 -> result: " + calculate(q2) + "\n");
+        System.out.println("Expected: 4.0 -> result: " + calculate(q2));
+        System.out.println();
 
         String q3 = "1 + 2 + 3";
         System.out.println(q3);
-        System.out.println("Expected: 6.0 -> result: " + calculate(q3) + "\n");
+        System.out.println("Expected: 6.0 -> result: " + calculate(q3));
+        System.out.println();
 
         String q4 = "6 / 2";
         System.out.println(q4);
-        System.out.println("Expected: 3.0 -> result: " + calculate(q4) + "\n");
+        System.out.println("Expected: 3.0 -> result: " + calculate(q4));
+        System.out.println();
 
         String q5 = "11 + 23";
         System.out.println(q5);
-        System.out.println("Expected: 34.0 -> result: " + calculate(q5) + "\n");
+        System.out.println("Expected: 34.0 -> result: " + calculate(q5));
+        System.out.println();
 
         String q6 = "11.1 + 23";
         System.out.println(q6);
-        System.out.println("Expected: 34.1 -> result: " + calculate(q6) + "\n");
+        System.out.println("Expected: 34.1 -> result: " + calculate(q6));
+        System.out.println();
 
         String q7 = "1 + 1 * 3";
         System.out.println(q7);
-        System.out.println("Expected: 4.0 -> result: " + calculate(q7) + "\n");
+        System.out.println("Expected: 4.0 -> result: " + calculate(q7));
+        System.out.println();
 
         String q8 = "( 11.5 + 15.4 ) + 10.1";
         System.out.println(q8);
-        System.out.println("Expected: 37.0 -> result: " + calculate(q8) + "\n");
+        System.out.println("Expected: 37.0 -> result: " + calculate(q8));
+        System.out.println();
 
         String q9 = "23 - ( 29.3 - 12.5 )";
         System.out.println(q9);
-        System.out.println("Expected: 6.2 -> result: " + calculate(q9) + "\n");
+        System.out.println("Expected: 6.2 -> result: " + calculate(q9));
+        System.out.println();
 
         String q10 = "10 - ( 2 + 3 * ( 7 - 5 ) )";
         System.out.println(q10);
